a8799639bda5e4ced78b53e629da5696c1208b70
Dariush <dariush@digi-x.my>
2019-05-15 10:12:36 +0800

    Updates

---
+++ precache-manifest.5b8fbd823961ce3365f3d514dcd22dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097e9c934e17290f759a2fa2e7cb7b3c",
+    "url": "/first-task-react-button/index.html"
+  },
+  {
+    "revision": "c06bac9a6e6e356b681a",
+    "url": "/first-task-react-button/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c7b1dbf2bcd5127f287a",
+    "url": "/first-task-react-button/static/css/main.e156ad68.chunk.css"
+  },
+  {
+    "revision": "c06bac9a6e6e356b681a",
+    "url": "/first-task-react-button/static/js/2.1e16670b.chunk.js"
+  },
+  {
+    "revision": "c7b1dbf2bcd5127f287a",
+    "url": "/first-task-react-button/static/js/main.03b547b5.chunk.js"
+  },
+  {
+    "revision": "c6001e05d7b8fecc6fb7",
+    "url": "/first-task-react-button/static/js/runtime~main.020a7088.js"
+  }
+]);
